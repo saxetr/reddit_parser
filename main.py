9b36ac36f8def323cd7099b47614638ca993275a
saxetr <saxetr@mail.ru>
2024-03-08 11:12:39 -0500

    version 1.0.1

--- main.py
+++ main.py
@@ -5,28 +5,12 @@
 import time
 import datetime
 from collections import Counter
-from typing import TypeAlias
 import praw
 from praw import models
 
-# https://support.reddithelp.com/hc/en-us/articles/16160319875092-Reddit-Data-API-Wiki
-# Monitor the following response headers to ensure that you're not exceeding the limits:
-#
-# X-Ratelimit-Used: Approximate number of requests used in this period
-# X-Ratelimit-Remaining: Approximate number of requests left to use
-# X-Ratelimit-Reset: Approximate number of seconds to end of period
-# We enforce rate limits for those eligible for free access usage of our Data API. The limit is:
-#
-# 100 queries per minute (QPM) per OAuth client id
-# QPM limits will be an average over a time window (currently 10 minutes) to support bursting requests.
-
-Subreddit: TypeAlias = praw.models.Subreddit
-Submission: TypeAlias = praw.models.Submission
-Comment: TypeAlias = praw.models.Comment
-MoreComments: TypeAlias = praw.models.MoreComments
 
 hours, minutes, seconds, mojo, bojo, dst_unknown = 0, 0, 0, 0, 0, -1
-current_unix_time = time.time()
+current_local_unix_time = time.time()
 today_local = datetime.date.today()
 two_days_ago_local = today_local - datetime.timedelta(days=2)
 two_days_ago_local_to_unix_time = time.mktime((two_days_ago_local.year,
@@ -47,63 +31,86 @@ def validate_subreddit_name(name: str):
     return True
 
 
-def check_is_subbredit_exist(name: str):
+def check_is_subbredit_exist(name: str) -> bool:
     pass
 
 
 reddit = praw.Reddit(site_name='parser_config', )
-subreddit = reddit.subreddit(ask_for_subreddit_name())
-
-big_submission = reddit.submission('1b83npr')
-r_test = reddit.submission('18da1zl')
 
 
-# TODO придерживаться 100 запросов в минуту, установить счетчик и задержку
-def fetch_submissions_for_period(subreddit: Subreddit, start: float, end: float) -> list[Submission]:
-    return [submission for submission in subreddit.new(limit=None) if start <= submission.created_utc <= end]
+def fetch_submissions_for_period(subreddit: models.Subreddit, start: float, end: float) -> list[models.Submission]:
+    submissions = []
+
+    for submission in subreddit.new(limit=None):
+        if start <= submission.created_utc <= end:
+            submissions.append(submission)
+            print(f'====fetch {len(submissions)} submissions in subreddit "{subreddit.title}" for the period from {two_days_ago_local} to {today_local}', end="\r", flush=True)
+    
+    print()
+     
+    return submissions
 
 
-def fetch_submissions_author_name(submissions):
+#todo что происходит с сабмишн после удаления автора?
+def fetch_submissions_author_name(submissions: list[models.Submission])-> list[str]:
     return [s.author.name for s in submissions]
 
 
-def more_comments_handler(more_comment: MoreComments):
+def load_inner_comments(more_comment: models.MoreComments)-> str:
     for comment in more_comment.comments():
         return get_comment_author_name(comment)
 
 
-def get_comment_author_name(comment) -> str:
-    if isinstance(comment, MoreComments):
-        return more_comments_handler(comment)
-    elif not isinstance(comment.author, type(None)):  # handling deleted users
-        return comment.author.name
-    return 'deleted!~!user'
+def get_comment_author_name(comment: models.Comment | models.MoreComments) -> str:
+    if isinstance(comment, models.MoreComments):
+        return load_inner_comments(comment)
+
+    if not comment.author:
+        return 'deleted~user'
+
+    return comment.author.name
 
 
-def generate_list_of_comment_author_names(submissions):
-    for s in submissions:
-        if s.num_comments:
-            for comment in s.comments.list():
-                # yield s.title, get_comment_author_name(comment)
-                yield get_comment_author_name(comment)
 
 
-def get_top_redditors(n: int, redditors_list):
+def generate_list_of_comment_author_names(submissions: list[models.Submission]):
+    return [get_comment_author_name(comment) for s in submissions if s.num_comments for comment in s.comments.list()]
+
+
+def get_top_redditors(n: int, redditors_list: list[str])-> list[tuple[str, int]]:
+    if n > len(redditors_list):
+        raise ValueError
+    
     return [r for r in Counter(redditors_list).most_common(n)]
 
 
-def get_top_commentators(n: int, commentators_list):
+def get_top_commentators(n: int, commentators_list: list[tuple[str, int]]):
+    if n > len(commentators_list):
+        raise ValueError
+    
     return [c for c in Counter(commentators_list).most_common(n)]
 
 
 if __name__ == '__main__':
-    # print(get_top_redditors(3, fetch_submissions_author_name(
-    #     fetch_submissions_for_period(subreddit, two_days_ago_local_to_unix_time, current_unix_time))))
+    subreddit = reddit.subreddit(ask_for_subreddit_name())
 
-    # print(get_top_commentators(3, generate_list_of_comment_author_names(
-    #     fetch_submissions_for_period(subreddit, two_days_ago_local_to_unix_time, current_unix_time))))
+    # big_submission = reddit.submission('1b83npr')
+    # r_test = reddit.submission('18da1zl')
+    # r_tt = reddit.submission('1b8o7li')
+
+    submissions_for_period = fetch_submissions_for_period(subreddit, two_days_ago_local_to_unix_time, current_local_unix_time)
+    top_n = 3
+
+    authors_of_submissions = fetch_submissions_author_name(submissions_for_period)
+    top_redditors = get_top_redditors(top_n, authors_of_submissions)
+    print(f'Top {top_n} redditors from {len(Counter(authors_of_submissions))}\n\t\t{top_redditors}')
+
+
+    list_of_comment_author_names = generate_list_of_comment_author_names(submissions_for_period)
+    top_commentators = get_top_commentators(3, list_of_comment_author_names)
+    print(f'Top {top_n} commentators from {len(Counter(list_of_comment_author_names))}\n\t\t{top_commentators}')
 
     # for x in generate_list_of_comment_author_names([r_test]):
     #     print(x)
 
-    print(get_top_commentators(3, generate_list_of_comment_author_names([r_test])))
+    # print(get_top_commentators(3, generate_list_of_comment_author_names([r_tt])))
